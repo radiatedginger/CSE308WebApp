ef4f410d0e6cd5ac52b36e70ca62393e2f862a10
Josh <jttrapper@gmail.com>
2019-01-19 22:36:48 -0800

    web app w/ txt field

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Bar from './Modules/appbar.js';
+import Txt1 from './Modules/txt.js';
+
+ReactDOM.render(
+    <div>
+        <Bar></Bar>
+        <Txt1></Txt1>
+    </div>
+    , document.getElementById('root'));
+
